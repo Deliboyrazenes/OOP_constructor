4ee624c871caf9a9dd82f38b4b6574e12bb1f851
Deliboyrazenes <enesdeliboyraz@gmail.com>
2024-10-26 12:57:52 +0300

    PersonClassAdded

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-    Person person = new Person("John","Doe",20);
+    Person person = new Person("John","Doe",15);
         System.out.println("Firstname: " + person.getFirstName());
         System.out.println("lastName: " + person.getLastName());
         System.out.println("Age: " + person.getAge());
